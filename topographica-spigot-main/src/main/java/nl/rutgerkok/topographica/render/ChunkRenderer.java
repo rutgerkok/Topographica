213d0329150d09eecda9871f7c97cd66559195f7
Rutger Kok <rutger.kok@live.com>
2018-02-03 16:29:29 +0100

    Improve the look of maps a bit

--- topographica-spigot-main/src/main/java/nl/rutgerkok/topographica/render/ChunkRenderer.java
+++ topographica-spigot-main/src/main/java/nl/rutgerkok/topographica/render/ChunkRenderer.java
@@ -6,11 +6,11 @@ import static nl.rutgerkok.topographica.util.SizeConstants.PIXEL_SIZE_BLOCKS;
 
 import java.util.Objects;
 
-import nl.rutgerkok.topographica.config.ColorConfig;
-
 import org.bukkit.ChunkSnapshot;
 import org.bukkit.Color;
 import org.bukkit.Material;
+
+import nl.rutgerkok.topographica.config.ColorConfig;
 
 public class ChunkRenderer {
 
@@ -45,25 +45,84 @@ public class ChunkRenderer {
     }
 
     protected Color adjustColor(Color color, int y) {
-        if (y > 74) {
-            // Make lighter
-            if (y > 194) {
-                y = 194;
-            }
-            double adjustment = (y - 74) / 200;
-            return makeLighter(color, adjustment);
+        // Clamp to 10 - 190
+        if (y > 190) {
+            y = 190;
+        } else if (y < 10) {
+            y = 10;
         }
+
+        // Rescale from 10 - 190 to 0 - 1
+        double adjustment = (y - 10) / 180.0;
+        if (adjustment < 0.49) {
+            return makeDarker(color, adjustment + 0.5);
+        } else if (adjustment > 0.51) {
+            return makeLighter(color, adjustment - 0.5);
+        }
+
         return color;
+    }
+
+    /**
+     * This method moves the y to the bottom of the sea, so that the sea can be
+     * made darker.
+     * 
+     * @param chunk
+     *            The chunk.
+     * @param x
+     *            X pos in the chunk.
+     * @param y
+     *            Y pos in the chunk.
+     * @param z
+     *            Z pos in the chunk.
+     * @return The y pos of the lowest liquid block.
+     */
+    protected final int fixYForLiquid(ChunkSnapshot chunk, int x, int y, int z) {
+        while (y > 0) {
+            y--;
+            Material material = getMaterial(chunk, x, y, z);
+            if (!isLiquid(material)) {
+                return y + 1;
+            }
+        }
+        return y;
+    }
+
+    @SuppressWarnings("deprecation")
+    protected Material getMaterial(ChunkSnapshot chunk, int x, int y, int z) {
+        // New methods are not yet available in Minecraft 1.8 - 1.11
+        return Material.getMaterial(chunk.getBlockTypeId(x, y, z));
+    }
+
+    protected boolean isLiquid(Material material) {
+        // New material names are not yet available in Minecraft 1.8 - 1.12
+        return material == Material.WATER || material == Material.STATIONARY_WATER;
+    }
+
+    /**
+     * Makes the color darker.
+     *
+     * @param color
+     *            The original color.
+     * @param amount
+     *            Amount: 1 is no change, 0 is completely black.
+     * @return The adjusted color.
+     */
+    private Color makeDarker(Color color, double adjustment) {
+        int red = (int) (color.getRed() * adjustment);
+        int green = (int) (color.getGreen() * adjustment);
+        int blue = (int) (color.getBlue() * adjustment);
+        return Color.fromRGB(red, green, blue);
     }
 
     /**
      * Makes the color lighter.
      *
      * @param color
-     *            The color to chane.
+     *            The original color.
      * @param amount
      *            Amount: 0 is no change, 1 is completely white.
-     * @return
+     * @return The adjusted color.
      */
     private Color makeLighter(Color color, double amount) {
         int red = (int) (color.getRed() + (amount * (255 - color.getRed())));
@@ -80,17 +139,17 @@ public class ChunkRenderer {
      * @param canvas
      *            Canvas to render to.
      */
-    @SuppressWarnings("deprecation")
     public void render(ChunkSnapshot chunk, Canvas canvas) {
         for (int x = 0; x < CHUNK_SIZE_BLOCKS; x += PIXEL_SIZE_BLOCKS) {
             for (int z = 0; z < CHUNK_SIZE_BLOCKS; z += PIXEL_SIZE_BLOCKS) {
                 int y = chunk.getHighestBlockYAt(x, z);
-                int blockId = chunk.getBlockTypeId(x, y, z);
-                if (blockId == 0 && y > 0) {
+                Material material = getMaterial(chunk, x, y, z);
+                if (material == Material.AIR && y > 0) {
                     // Air, try one block lower
-                    blockId = chunk.getBlockTypeId(x, y - 1, z);
+                    y--;
+                    material = getMaterial(chunk, x, y, z);
                 }
-                Material material = Material.getMaterial(blockId);
+                y = fixYForLiquid(chunk, x, y, z);
 
                 int worldX = chunk.getX() << CHUNK_SIZE_BLOCKS_BITS | x;
                 int worldZ = chunk.getZ() << CHUNK_SIZE_BLOCKS_BITS | z;
--- topographica-spigot-modern/src/main/java/nl/rutgerkok/topographica/render/ChunkRendererModern.java
+++ topographica-spigot-modern/src/main/java/nl/rutgerkok/topographica/render/ChunkRendererModern.java
@@ -1,13 +1,9 @@
 package nl.rutgerkok.topographica.render;
 
-import static nl.rutgerkok.topographica.util.SizeConstants.CHUNK_SIZE_BLOCKS;
-import static nl.rutgerkok.topographica.util.SizeConstants.CHUNK_SIZE_BLOCKS_BITS;
-import static nl.rutgerkok.topographica.util.SizeConstants.PIXEL_SIZE_BLOCKS;
-
-import nl.rutgerkok.topographica.config.ColorConfig;
-
 import org.bukkit.ChunkSnapshot;
 import org.bukkit.Material;
+
+import nl.rutgerkok.topographica.config.ColorConfig;
 
 public final class ChunkRendererModern extends ChunkRenderer {
 
@@ -16,21 +12,12 @@ public final class ChunkRendererModern extends ChunkRenderer {
     }
 
     @Override
-    public void render(ChunkSnapshot chunk, Canvas canvas) {
-        for (int x = 0; x < CHUNK_SIZE_BLOCKS; x += PIXEL_SIZE_BLOCKS) {
-            for (int z = 0; z < CHUNK_SIZE_BLOCKS; z += PIXEL_SIZE_BLOCKS) {
-                int y = chunk.getHighestBlockYAt(x, z);
-                Material material = chunk.getBlockType(x, y, z);
-                if (material == Material.AIR && y > 0) {
-                    // Air, try one block lower
-                    material = chunk.getBlockType(x, y - 1, z);
-                }
+    protected Material getMaterial(ChunkSnapshot chunk, int x, int y, int z) {
+        return chunk.getBlockType(x, y, z);
+    }
 
-                int worldX = chunk.getX() << CHUNK_SIZE_BLOCKS_BITS | x;
-                int worldZ = chunk.getZ() << CHUNK_SIZE_BLOCKS_BITS | z;
-                canvas.setColor(worldX, worldZ, adjustColor(colorMap.getColor(material), y));
-            }
-        }
-
+    @Override
+    protected boolean isLiquid(Material material) {
+        return material == Material.WATER || material == Material.FLOWING_WATER;
     }
 }
