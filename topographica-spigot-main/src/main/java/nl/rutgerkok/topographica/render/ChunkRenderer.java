2c5a92a7f4ed14e8e8dd6059115ec8e3dee4548c
Rutger Kok <rutger.kok@live.com>
2018-05-30 21:46:55 +0200

    Add shades

--- topographica-spigot-main/src/main/java/nl/rutgerkok/topographica/render/ChunkRenderer.java
+++ topographica-spigot-main/src/main/java/nl/rutgerkok/topographica/render/ChunkRenderer.java
@@ -7,11 +7,12 @@ import static nl.rutgerkok.topographica.util.SizeConstants.PIXEL_SIZE_BLOCKS;
 
 import java.util.Objects;
 
-import nl.rutgerkok.topographica.config.WorldConfig;
-
 import org.bukkit.ChunkSnapshot;
 import org.bukkit.Color;
 import org.bukkit.Material;
+
+import nl.rutgerkok.topographica.config.ColorConfig;
+import nl.rutgerkok.topographica.config.WorldConfig;
 
 public class ChunkRenderer {
 
@@ -45,7 +46,7 @@ public class ChunkRenderer {
         this.worldConfig = Objects.requireNonNull(worldConfig, "worldConfig");
     }
 
-    protected Color adjustColor(Color color, int y) {
+    protected Color adjustColorForHeight(Color color, int y) {
         // Clamp to 10 - 190
         if (y > 190) {
             y = 190;
@@ -64,29 +65,39 @@ public class ChunkRenderer {
         return color;
     }
 
-    /**
-     * This method moves the y to the bottom of the sea, so that the sea can be
-     * made darker.
-     *
-     * @param chunk
-     *            The chunk.
-     * @param x
-     *            X pos in the chunk.
-     * @param y
-     *            Y pos in the chunk.
-     * @param z
-     *            Z pos in the chunk.
-     * @return The y pos of the lowest liquid block.
-     */
-    protected final int fixYForLiquid(ChunkSnapshot chunk, int x, int y, int z) {
+    private Color adjustColorForHeightChange(Color color, int y, int yNextColumn) {
+        if (yNextColumn > y) {
+            return makeLighter(color, 0.1);
+        } else if (yNextColumn < y) {
+            return makeDarker(color, 0.9);
+        }
+        return color;
+    }
+
+    private int getHighestY(ChunkSnapshot chunk, int x, int z) {
+        int y = chunk.getHighestBlockYAt(x, z);
+        if (y >= CHUNK_HEIGHT_BLOCKS) {
+            return CHUNK_HEIGHT_BLOCKS - 1;
+        }
+        if (y <= 0) {
+            return 0;
+        }
+        Material material = getMaterial(chunk, x, y, z);
+        if (material == Material.AIR) {
+            // Air, try one block lower
+            y--;
+        }
+
+        // Move down if we found a liquid block
         while (y > 0) {
             y--;
-            Material material = getMaterial(chunk, x, y, z);
+            material = getMaterial(chunk, x, y, z);
             if (!isLiquid(material)) {
                 return y + 1;
             }
         }
-        return y;
+
+        return 0;
     }
 
     @SuppressWarnings("deprecation")
@@ -141,6 +152,7 @@ public class ChunkRenderer {
      *            Canvas to render to.
      */
     public void render(ChunkSnapshot chunk, Canvas canvas) {
+        ColorConfig colors = worldConfig.getColors();
         for (int x = 0; x < CHUNK_SIZE_BLOCKS; x += PIXEL_SIZE_BLOCKS) {
             for (int z = 0; z < CHUNK_SIZE_BLOCKS; z += PIXEL_SIZE_BLOCKS) {
                 int worldX = chunk.getX() << CHUNK_SIZE_BLOCKS_BITS | x;
@@ -148,19 +160,16 @@ public class ChunkRenderer {
                 if (!this.worldConfig.getRenderArea().shouldRenderColumn(worldX, worldZ)) {
                     continue;
                 }
-                int y = chunk.getHighestBlockYAt(x, z);
-                if (y >= CHUNK_HEIGHT_BLOCKS) {
-                    y = CHUNK_HEIGHT_BLOCKS - 1;
-                }
+                int y = getHighestY(chunk, x, z);
                 Material material = getMaterial(chunk, x, y, z);
-                if (material == Material.AIR && y > 0) {
-                    // Air, try one block lower
-                    y--;
-                    material = getMaterial(chunk, x, y, z);
-                }
-                y = fixYForLiquid(chunk, x, y, z);
+                int yNextColumn = getHighestY(chunk, x, z + 1);
 
-                canvas.setColor(worldX, worldZ, adjustColor(worldConfig.getColors().getColor(material), y));
+                Color color = colors.getColor(material);
+                color = adjustColorForHeight(color, y);
+                if (!isLiquid(material)) {
+                    color = adjustColorForHeightChange(color, y, yNextColumn);
+                }
+                canvas.setColor(worldX, worldZ, color);
             }
         }
 
